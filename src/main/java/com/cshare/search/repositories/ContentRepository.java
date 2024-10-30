72f863c536b5c37f1ff54ba02fb553d6b73927c8
Emyr298 <emi.ru298@gmail.com>
2024-10-30 19:25:27 +0700

    feat: add fuzziness to search

--- src/main/java/com/cshare/search/repositories/ContentRepository.java
+++ src/main/java/com/cshare/search/repositories/ContentRepository.java
@@ -1,6 +1,7 @@
 package com.cshare.search.repositories;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.data.repository.reactive.ReactiveSortingRepository;
 
@@ -10,5 +11,23 @@ import reactor.core.publisher.Flux;
 
 public interface ContentRepository
         extends ReactiveSortingRepository<Content, String>, ReactiveCrudRepository<Content, String> {
+    @Query("""
+        {
+            "bool": {
+                "should": [
+                    {
+                        "fuzzy": {
+                            "title": "?0"
+                        }
+                    },
+                    {
+                        "fuzzy": {
+                            "description": "?0"
+                        }
+                    }
+                ]
+            }
+        }
+    """)
     public Flux<Content> findByTitleOrDescription(String title, String description, Pageable pageable);
 }
