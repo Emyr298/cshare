80fefe28cc608b3a61f9c8b1a71d192f36269d8d
Emyr298 <emi.ru298@gmail.com>
2024-11-05 20:47:13 +0700

    fix: config

--- infrastructure/init.py
+++ infrastructure/init.py
@@ -1,13 +1,23 @@
-import os, json
+import dotenv
+dotenv.load_dotenv()
+
+import os, json, time
+
+print(os.environ.get('IS_ENV_LOADED'))
 
 def init_debezium():
     with open('config/debezium.conf', 'r') as file:
-        conf = file.read()
+        conf = file.read().replace(
+                '${POSTGRES_USER}', os.environ.get('POSTGRES_USER')
+            ).replace(
+                '${POSTGRES_PASSWORD}', os.environ.get('POSTGRES_PASSWORD')
+            )
         conf = json.dumps(json.loads(conf))
-    # print(conf)
     cmd = f'curl -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d \'{conf}\''
     docker_cmd = f"docker exec -it cshare-debezium-connector {cmd}"
     os.system(docker_cmd)
 
 if __name__ == '__main__':
+    os.system('docker compose up -d')
+    time.sleep(10)
     init_debezium()
