bc074f506603c19c94e7b40eee39c00c48e60d98
Emyr298 <emi.ru298@gmail.com>
2024-11-14 22:03:39 +0700

    feat: add navbar

--- src/components/elements/NavigationBar/index.tsx
+++ src/components/elements/NavigationBar/index.tsx
@@ -1,8 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Home, Users } from "lucide-react";
+import { SearchBar } from "../SearchBar";
+import { ProfileButton } from "../ProfileButton";
+import Link from "next/link";
+
 export default function NavigationBar() {
-    return (
-      <div>
-          <h1>Test</h1>
+  return (
+    <div className="w-full px-64 py-2 border-b-2 border-secondary bg-primary-foreground">
+      <div className="flex justify-between items-center">
+        <div className="flex flex-row items-center gap-4">
+          <Link href="/" className="text-xl font-bold">CShare</Link>
+          <SearchBar />
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <Button variant="ghost" size="icon">
+            <Home />
+          </Button>
+          <Button variant="ghost" size="icon">
+            <Users />
+          </Button>
+          <ProfileButton />
+        </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
