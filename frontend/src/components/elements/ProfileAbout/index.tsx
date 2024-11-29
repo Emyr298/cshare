3795730718dc25dbc24ebc20a7263bfec8986083
Emyr298 <emi.ru298@gmail.com>
2024-11-29 09:04:21 +0700

    fix: text sizing

--- frontend/src/components/elements/ProfileAbout/index.tsx
+++ frontend/src/components/elements/ProfileAbout/index.tsx
@@ -7,8 +7,8 @@ export const ProfileAbout: React.FC<ProfileAboutProps> = ({ username }) => {
       <div className="w-full py-1 px-2 bg-secondary">
         <h2 className="font-bold">About Fligger</h2>
       </div>
-      <div className="py-1 px-2">
-        <p>
+      <div className="px-4 py-2">
+        <p className="text-[0.95rem]">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
